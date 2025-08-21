d5255bdd8a7e85e873e85e647f63ca7b110ea55f
111031075jack <111031075@live.asia.edu.tw>
2025-08-21 19:37:11 +0800

    update

--- src/aes/AESSample.java
+++ src/aes/AESSample.java
@@ -2,6 +2,13 @@ package aes;
 
 public class AESSample {
 	
-	
+	public static void main(String[] args) {
+		// 明文
+		String originalText = "行政院宣布全民發放一萬元";
+		System.out.printf("明文: %s%n", originalText);
+		System.out.println("-------------------------");
+		
+		
+	}
 	
 }
